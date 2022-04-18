a9275d372fcbd7f97ef8d830ae2ff27722f8832b
anagonzalezalvarez <ana.gonzalezalvarez@kaust.edu.sa>
2022-04-18 13:50:04 +0300

    initial commit

---
+++ R/fnAreaCircle.R
@@ -0,0 +1,12 @@
+#' Calculate area of circle
+#'
+#' Given a colour make n lighter or darker shades
+#'
+#' @param r radius of the circle
+#'
+#' @export
+fnAreaCircle <- function(r){
+  area <- pi*r^2
+  return(area)
+}
+
